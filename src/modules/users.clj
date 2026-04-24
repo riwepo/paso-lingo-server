1a76899b3183e9dabcc709950ade4a81326dea02
riwepo <riwepo.coding@gmail.com>
2026-04-23 21:42:38 -0300

    add user depot and pstates

---
+++ src/modules/users.clj
@@ -0,0 +1,44 @@
+(ns modules.users
+  (:use [com.rpl.rama]
+        [com.rpl.rama path]))
+
+;; Event records
+(defrecord NewUser [user-id role preferences])
+(defrecord UpdateUserRole [user-id role])
+(defrecord UpdateUserPreferences [user-id preferences])
+
+(defmodule UserDetailsModule
+  [setup topologies]
+  ;; Depot partitioned by user-id
+  (declare-depot setup *users-depot (hash-by :user-id))
+
+  (let [s (stream-topology topologies "users")]
+    ;; Persistent state: map from user-id -> fixed record
+    (declare-pstate
+      s
+      $$users
+      {String (fixed-keys-schema
+                {:role #{:guest :member :admin}
+                 :preferences map})})
+
+    ;; Sources: consume depot events
+    (<<sources s
+      (source> *users-depot :> *data)
+
+      ;; New membership creation
+      (<<subsource *data
+                   (case> NewUser :> {:keys [*user-id *role *preferences]})
+                   (local-transform> [(keypath *user-id)
+                                      (termval {:role *role
+                                                :preferences *preferences})]
+                                $$users)
+
+                   ;; Role update
+                   (case> UpdateUserRole :> {:keys [*user-id *role]})
+                   (local-transform> [(must *user-id) :role (termval *role)]
+                                $$users)
+
+                   ;; Preferences update (merge map)
+                   (case> UpdateUserPreferences :> {:keys [*user-id *preferences]})
+                   (local-transform> [(must *user-id) :preferences (mergeval *preferences)]
+                                $$users)))))
